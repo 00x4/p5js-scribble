fe3f0dad124dc66bee2a61e9c9ab5ca9888b999c
Yoshito Yamazaki <dishhh@gmail.com>
2020-08-25 20:22:37 +0900

    Clear sketch

--- sketch.js
+++ sketch.js
@@ -1,65 +1,6 @@
-let
-  _dotColor,
-  _isEllipse,
-  _counter;
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background(255);
-  stroke(150);
-  fill(255);
-  rectMode(CENTER);
-  ellipseMode(CENTER);
-  _dotColor = color(random(230, 255), random(230, 255), random(230, 255));
-  _isEllipse = false;
-  _counter = 0;
+  createCanvas(800, 800);
 }
 
 function draw() {
-  _dotColor = changeColorLittleBit(_dotColor);
-  fill(_dotColor);
-  stroke(150);
-  strokeWeight(1);
-  if (random(100) < 50) {
-    _isEllipse = !_isEllipse;
-  }
-  drawCloud();
-  if (_counter ++ > 10) {
-    drawSmoke();
-    _counter = 0;
-  }
 }
-
-const negativeHalf = function(v) {
-  return (random(1) > 0.5) ? v : v * -1;
-};
-
-const changeColorLittleBit = (c) => {
-  return color(
-    min(max(red(c) + random(-3, 3), 230), 255),
-    min(max(green(c) + random(-3, 3), 230), 255),
-    min(max(blue(c) + random(-3, 3), 230), 255)
-  );
-};
-
-const drawCloud = () => {
-  blendMode(random(100) < 50 ? BLEND : MULTIPLY);
-  for (let i = 0; i < 50; ++i) {
-    const
-      size = max(10, random(80)),
-      x = random(width),
-      y = random(height);
-    if (_isEllipse) {
-      ellipse(x, y, size, size);
-    } else {
-      rect(x, y, size, size);
-    }
-  }
-};
-
-const drawSmoke = () => {
-  blendMode(BLEND);
-  fill(color(255, 30));
-  noStroke();
-  rect(width / 2, height / 2, width, height);
-};
